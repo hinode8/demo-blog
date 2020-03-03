04d0a4bd06b4cb20bc0d669049a8be99d4addbb1
Shinjiro Tomiyoshi <shinjiro.tomiyoshi@g.samrtvalue.ad.jp>
2020-03-03 16:31:47 +0900

    form field is added

--- code/web/src/modules/pages/NewPost.js
+++ code/web/src/modules/pages/NewPost.js
@@ -1,51 +1,105 @@
 // Imports
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Helmet } from 'react-helmet'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
 // UI Imports
-import { Grid, GridCell } from '../../ui/grid'
-import { H2, H5 } from '../../ui/typography'
-import ImageTile from '../../ui/image/Tile'
+import { H2, H5 } from '../../ui/typography';
+import { Grid, GridCell } from '../../ui/grid';
+import ImageTile from '../../ui/image/Tile';
+import Input from '../../ui/input/Input';
 
 // App Imports
-import { APP_URL } from '../../setup/config/env'
+import { APP_URL } from '../../setup/config/env';
 
 // Component
-const NewPost = (props) => (
-  <Grid alignCenter={true} style={{ padding: '2em' }}>
-    {/* SEO */}
-    <Helmet>
-      <title>Monthly supply of clothes and accessories for Women - Crate</title>
-    </Helmet>
+class NewPost extends Component {
+  constructor(props) {
+    super(props);
 
-    {/* Content */}
-    <GridCell style={{ textAlign: 'center' }}>
-      <Grid>
-        <GridCell justifyCenter={true}>
-          <ImageTile width={120} height={120} style={{marginRight:"Auto", marginLeft:"Auto"}} image={`${APP_URL}/images/stock/newpost/1.png`} />
-        </GridCell>
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+
+    // Function bindings
+  }
+
+  onChange = event => {};
+
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
+  render() {
+    const { isLoading, error } = this.props.user;
+
+    return (
+      <Grid gutter={true} alignCenter={true} style={{ padding: '2em' }}>
+        <Grid alignCenter={true} style={{ width: '100%' }}>
+          {/* SEO */}I
+          <Helmet>
+            <title>
+              Monthly supply of clothes and accessories for Women - Crate
+            </title>
+          </Helmet>
+          {/* Content */}
+          <GridCell style={{ textAlign: 'center' }}>
+            <Grid>
+              <GridCell justifyCenter={true}>
+                <ImageTile
+                  width={120}
+                  height={120}
+                  style={{ marginRight: 'Auto', marginLeft: 'Auto' }}
+                  image={`${APP_URL}/images/stock/newpost/1.png`}
+                />
+              </GridCell>
+            </Grid>
+            <H2 font="secondary">Create a Post</H2>
+
+            <H5 style={{ marginTop: '0.5em' }}>
+              Keep it real and make the world better place!
+            </H5>
+          </GridCell>
+        </Grid>
+        <Grid alignCenter={true} style={{ width: '100%' }}>
+          <GridCell style={{ textAlign: 'center' }}>
+            {/* Login Form */}
+            <form onSubmit={this.onSubmit}>
+              <div style={{ width: '25em', margin: '0 auto' }}>
+                {/* Email */}
+                <Input
+                  type="email"
+                  fullWidth={true}
+                  placeholder="Email"
+                  required="required"
+                  name="email"
+                  value={this.state.user.email}
+                  onChange={this.onChange}
+                  style={{ marginTop: '1em' }}
+                />
+
+              </div>
+            </form>
+          </GridCell>
+        </Grid>
       </Grid>
-      <H2 font="secondary">Create a Post</H2>
-
-      <H5 style={{ marginTop: '0.5em' }}>Keep it real and make the world better place!</H5>
-
-    </GridCell>
-  </Grid>
-)
-
+    );
+  }
+}
 // Component Properties
 NewPost.propTypes = {
-  user: PropTypes.object.isRequired
-}
+  user: PropTypes.object.isRequired,
+};
 
 // Component State
 function newPostState(state) {
   return {
-    user: state.user
-  }
+    user: state.user,
+  };
 }
 
-export default connect(newPostState, {})(NewPost)
+export default connect(newPostState, {})(NewPost);
