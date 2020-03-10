b405728c2cf8bc90f6c687579e5a952b3f246d8c
Shinjiro Tomiyoshi <shinjiro.tomiyoshi@g.samrtvalue.ad.jp>
2020-03-10 10:37:32 +0900

    semicolons are added

--- code/web/src/modules/admin/crate/CreateOrEdit.js
+++ code/web/src/modules/admin/crate/CreateOrEdit.js
@@ -1,116 +1,118 @@
 // Imports
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Helmet } from 'react-helmet'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { Link, withRouter } from 'react-router-dom';
 
 // UI Imports
-import { Grid, GridCell } from '../../../ui/grid'
-import Button from '../../../ui/button'
-import Icon from '../../../ui/icon'
-import H4 from '../../../ui/typography/H4'
-import { Input, Textarea } from '../../../ui/input'
-import { white } from "../../../ui/common/colors"
+import { Grid, GridCell } from '../../../ui/grid';
+import Button from '../../../ui/button';
+import Icon from '../../../ui/icon';
+import H4 from '../../../ui/typography/H4';
+import { Input, Textarea } from '../../../ui/input';
+import { white } from '../../../ui/common/colors';
 
 // App Imports
-import admin from '../../../setup/routes/admin'
-import { slug } from '../../../setup/helpers'
+import admin from '../../../setup/routes/admin';
+import { slug } from '../../../setup/helpers';
 import {
   createOrUpdate as crateCreateOrUpdate,
-  getById as getCrateById
-} from '../../crate/api/actions'
-import { messageShow, messageHide } from '../../common/api/actions'
-import AdminMenu from '../common/Menu'
+  getById as getCrateById,
+} from '../../crate/api/actions';
+import { messageShow, messageHide } from '../../common/api/actions';
+import AdminMenu from '../common/Menu';
 
 // Component
 class CreateOrEdit extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isLoading: false,
       crate: {
         id: 0,
         name: '',
-        description: ''
+        description: '',
       },
       crateTypes: [],
       userGenders: [],
-    }
+    };
   }
 
   componentDidMount() {
     // Get crate details (edit case)
-    this.getCrate(parseInt(this.props.match.params.id))
+    this.getCrate(parseInt(this.props.match.params.id));
   }
 
-  getCrate = (crateId) => {
+  getCrate = crateId => {
     if (crateId > 0) {
-      this.props.getCrateById(crateId)
+      this.props
+        .getCrateById(crateId)
         .then(response => {
           if (response.data.errors && response.data.errors.length > 0) {
-            this.props.messageShow(response.data.errors[0].message)
+            this.props.messageShow(response.data.errors[0].message);
           } else {
             this.setState({
-              crate: response.data.data.crateById
-            })
+              crate: response.data.data.crateById,
+            });
           }
         })
         .catch(error => {
-          this.props.messageShow('There was some error fetching crate types. Please try again.')
-        })
+          this.props.messageShow(
+            'There was some error fetching crate types. Please try again.',
+          );
+        });
     }
-  }
+  };
 
-  onChange = (event) => {
-    let crate = this.state.crate
-    crate[event.target.name] = event.target.value
-
-    this.setState({
-      crate
-    })
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault()
+  onChange = event => {
+    let crate = this.state.crate;
+    crate[event.target.name] = event.target.value;
 
     this.setState({
-      isLoading: true
-    })
+      crate,
+    });
+  };
 
-    this.props.messageShow('Saving crate, please wait...')
+  onSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      isLoading: true,
+    });
+
+    this.props.messageShow('Saving crate, please wait...');
 
     // Save crate
-    this.props.crateCreateOrUpdate(this.state.crate)
+    this.props
+      .crateCreateOrUpdate(this.state.crate)
       .then(response => {
         this.setState({
-          isLoading: false
-        })
+          isLoading: false,
+        });
 
         if (response.data.errors && response.data.errors.length > 0) {
-          this.props.messageShow(response.data.errors[0].message)
+          this.props.messageShow(response.data.errors[0].message);
         } else {
-          this.props.messageShow('Crate saved successfully.')
+          this.props.messageShow('Crate saved successfully.');
 
-          this.props.history.push(admin.crateList.path)
+          this.props.history.push(admin.crateList.path);
         }
       })
       .catch(error => {
-        this.props.messageShow('There was some error. Please try again.')
+        this.props.messageShow('There was some error. Please try again.');
 
         this.setState({
-          isLoading: false
-        })
+          isLoading: false,
+        });
       })
       .then(() => {
         window.setTimeout(() => {
-          this.props.messageHide()
-        }, 5000)
-      })
-  }
+          this.props.messageHide();
+        }, 5000);
+      });
+  };
 
   render() {
     return (
@@ -121,7 +123,7 @@ class CreateOrEdit extends Component {
         </Helmet>
 
         {/* Top menu bar */}
-        <AdminMenu/>
+        <AdminMenu />
 
         {/* Page Content */}
         <div>
@@ -129,7 +131,9 @@ class CreateOrEdit extends Component {
           <Grid alignCenter={true} style={{ padding: '1em' }}>
             <GridCell style={{ textAlign: 'left' }}>
               <Link to={admin.crateList.path}>
-                <Button><Icon size={1.2}>arrow_back</Icon> Back</Button>
+                <Button>
+                  <Icon size={1.2}>arrow_back</Icon> Back
+                </Button>
               </Link>
             </GridCell>
           </Grid>
@@ -137,8 +141,12 @@ class CreateOrEdit extends Component {
           {/* Crate list */}
           <Grid alignCenter={true} style={{ padding: '1em' }}>
             <GridCell>
-              <H4 font="secondary" style={{ marginBottom: '1em', textAlign: 'center' }}>
-                {this.props.match.params.id === undefined ? 'Create' : 'Edit'} Crate
+              <H4
+                font="secondary"
+                style={{ marginBottom: '1em', textAlign: 'center' }}
+              >
+                {this.props.match.params.id === undefined ? 'Create' : 'Edit'}{' '}
+                Crate
               </H4>
 
               {/* Form */}
@@ -170,8 +178,15 @@ class CreateOrEdit extends Component {
 
                 {/* Form submit */}
                 <div style={{ marginTop: '2em', textAlign: 'center' }}>
-                  <Button type="submit" theme="secondary" disabled={this.state.isLoading}>
-                    <Icon size={1.2} style={{ color: white }}>check</Icon> Save
+                  <Button
+                    type="submit"
+                    theme="secondary"
+                    disabled={this.state.isLoading}
+                  >
+                    <Icon size={1.2} style={{ color: white }}>
+                      check
+                    </Icon>{' '}
+                    Save
                   </Button>
                 </div>
               </form>
@@ -179,7 +194,7 @@ class CreateOrEdit extends Component {
           </Grid>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -188,12 +203,14 @@ CreateOrEdit.propTypes = {
   crateCreateOrUpdate: PropTypes.func.isRequired,
   getCrateById: PropTypes.func.isRequired,
   messageShow: PropTypes.func.isRequired,
-  messageHide: PropTypes.func.isRequired
-}
+  messageHide: PropTypes.func.isRequired,
+};
 
-export default withRouter(connect(null, {
-  crateCreateOrUpdate,
-  getCrateById,
-  messageShow,
-  messageHide
-})(CreateOrEdit))
+export default withRouter(
+  connect(null, {
+    crateCreateOrUpdate,
+    getCrateById,
+    messageShow,
+    messageHide,
+  })(CreateOrEdit),
+);
