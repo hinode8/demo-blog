8cfb5ad259bf5e42d32d183e53fa43f6d061f340
Shinjiro Tomiyoshi <shinjiro.tomiyoshi@g.samrtvalue.ad.jp>
2020-03-24 17:00:12 +0900

    delete unused files and routes

--- code/web/src/setup/routes/home.js
+++ code/web/src/setup/routes/home.js
@@ -1,8 +1,5 @@
 // App Imports
 import Home from '../../modules/pages/Home'
-import Posts from '../../modules/pages/Posts'
-import NewPost from '../../modules/pages/NewPost'
-import HowItWorks from '../../modules/pages/HowItWorks'
 import WhatsNew from '../../modules/pages/WhatsNew'
 
 // Home routes
@@ -11,17 +8,7 @@ export default {
     path: '/',
     component: Home,
     exact: true
-  },
-
-  posts: {
-    path: '/posts',
-    component: Posts
-  },
-
-  newPost: {
-    path: '/new-post',
-    component: NewPost
-  },
+  }, 
 
   whatsNew: {
     path: '/whats-new',
