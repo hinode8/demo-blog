f475e062c5368ac1773e749e18db645d95663ddf
Shinjiro Tomiyoshi <shinjiro.tomiyoshi@g.samrtvalue.ad.jp>
2020-03-26 16:12:32 +0900

    [web] blog list page is created

--- code/web/src/modules/blog/List.js
+++ code/web/src/modules/blog/List.js
@@ -1,60 +1,190 @@
 // Imports
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Helmet } from 'react-helmet'
-import { Link } from 'react-router-dom'
-
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { Helmet } from 'react-helmet';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import admin from '../../setup/routes/admin';
+import { black } from '../../ui/common/colors';
+import { Grid, GridCell } from '../../ui/grid';
+import Icon from '../../ui/icon';
 // UI Imports
-import { Grid, GridCell } from '../../ui/grid'
-import { H2, H5 } from '../../ui/typography'
-import Button from '../../ui/button'
-import ImageTile from '../../ui/image/Tile'
-import { level1 } from '../../ui/common/shadows'
-
+import { H2 } from '../../ui/typography';
+import { messageHide, messageShow } from '../common/api/actions';
+import EmptyMessage from '../common/EmptyMessage';
+import Loading from '../common/Loading';
 // App Imports
-import { APP_URL } from '../../setup/config/env'
-import crateRoutes from '../../setup/routes/crate'
-import userRoutes from '../../setup/routes/user'
+import { getList as getBlogPostList, remove as removeBlogPost } from './api/actions';
 
 // Component
-const Posts = (props) => (
-  <Grid alignCenter={true} style={{ padding: '2em' }}>
-    {/* SEO */}
-    <Helmet>
-      <title>Posts</title>
-    </Helmet>
+class List extends PureComponent {
+  // Runs on server only for SSR
+  static fetchData({ store }) {
+    return store.dispatch(getBlogPostList('DESC'));
+  }
 
+  // Runs on client only
+  componentDidMount() {
+    this.props.getBlogPostList('DESC');
+  }
 
-    <GridCell style={{ textAlign: 'center' }}>
-      <H2 font="secondary">Blog Posts</H2>
+  remove = id => {
+    if (id > 0) {
+      let check = confirm('Are you sure you want to delete this blogPost?');
 
-      <H5 style={{ marginTop: '0.5em' }}>list of blog posts</H5>
+      if (check) {
+        this.props.messageShow('Deleting, please wait...');
 
-      {/* Call to action */}
-      {
-        props.user.isAuthenticated
-          ? <Link to={crateRoutes.list.path}>
-              <Button theme="secondary" style={{ marginTop: '1em' }}>Get Subscription</Button>
-            </Link>
-          : <Link to={userRoutes.signup.path}>
-              <Button theme="secondary" style={{ marginTop: '1em' }}>Get Started</Button>
-            </Link>
+        this.props
+          .removeBlogPost({ id })
+          .then(response => {
+            if (response.status === 200) {
+              if (response.data.errors && response.data.errors.length > 0) {
+                this.props.messageShow(response.data.errors[0].message);
+              } else {
+                this.props.messageShow('BlogPost deleted successfully.');
+
+                this.props.getBlogPostList(false);
+              }
+            } else {
+              this.props.messageShow('Please try again.');
+            }
+          })
+          .catch(error => {
+            this.props.messageShow('There was some error. Please try again.');
+          })
+          .then(() => {
+            this.setState({
+              isLoading: false,
+            });
+
+            window.setTimeout(() => {
+              this.props.messageHide();
+            }, 5000);
+          });
       }
-    </GridCell>
-  </Grid>
-)
+    }
+  };
 
-// Component Properties
-Posts.propTypes = {
-  user: PropTypes.object.isRequired
-}
+  render() {
+    let blogPosts = this.props.blogPosts;
+    
+    const { isLoading, list } = this.props.blogPosts;
 
-// Component State
-function postsState(state) {
-  return {
-    user: state.user
+    return (
+      <div>
+        {/* SEO */}
+        <Helmet>
+          <title>BlogPosts - Admin - BlogPost</title>
+        </Helmet>
+
+        {/* Page Content */}
+        <Grid  alignCenter={true} >
+          <Grid alignCenter={true} style={{ width: '100%'}}>
+            <GridCell style={{ textAlign: 'center' }}>
+              <H2 font="secondary"> Blog Posts </H2>
+            </GridCell>
+          </Grid>
+
+          {/* BlogPost list */}
+          <Grid alignCenter={true} style={{ width: '100%', padding:'1em' }}>
+            <GridCell>
+              <table className="striped">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Content</th>
+                    <th>Created at</th>
+                    <th>Updated at</th>
+                    <th style={{ textAlign: 'center' }}>Actions</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {isLoading ? (
+                    <tr>
+                      <td colSpan="6">
+                        <Loading message="loading blogPosts..." />
+                      </td>
+                    </tr>
+                  ) : list.length > 0 ? (
+                    list.map(
+                      ({
+                        id,
+                        title,
+                        content,
+                        createdAt,
+                        updatedAt,
+                      }) => (
+                        <tr key={id}>
+                          <td>{title}</td>
+
+                          <td>{content}</td>
+
+                          <td>
+                            {new Date(parseInt(createdAt)).toDateString()}
+                          </td>
+
+                          <td>
+                            {new Date(parseInt(updatedAt)).toDateString()}
+                          </td>
+
+                          <td style={{ textAlign: 'center' }}>
+                            <Link to={admin.blogPostEdit.path(id)}>
+                              <Icon size={2} style={{ color: black }}>
+                                mode_edit
+                              </Icon>
+                            </Link>
+
+                            <span
+                              style={{ cursor: 'pointer' }}
+                              onClick={this.remove.bind(this, id)}
+                            >
+                              <Icon size={2} style={{ marginLeft: '0.5em' }}>
+                                delete
+                              </Icon>
+                            </span>
+                          </td>
+                        </tr>
+                      ),
+                    )
+                  ) : (
+                    <tr>
+                      <td colSpan="6">
+                        <EmptyMessage message="No blogPosts to show." />
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </GridCell>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
-export default connect(postsState, {})(Posts)
+// Component Properties
+List.propTypes = {
+  blogPosts: PropTypes.object.isRequired,
+  getBlogPostList: PropTypes.func.isRequired,
+  removeBlogPost: PropTypes.func.isRequired,
+  messageShow: PropTypes.func.isRequired,
+  messageHide: PropTypes.func.isRequired,
+};
+
+// Component State
+function listState(state) {
+  console.log(state)
+  return {
+    blogPosts: state.blogPosts,
+  };
+}
+
+export default connect(listState, {
+  getBlogPostList,
+  removeBlogPost,
+  messageShow,
+  messageHide,
+})(List);
