f10d08ee823f98f16109d80ea85a85be7101598a
Shinjiro Tomiyoshi <shinjiro.tomiyoshi@g.samrtvalue.ad.jp>
2020-04-09 17:20:55 +0900

    [api] :: seeder for blogPosts

---
+++ code/api/src/seeders/4-blogPosts.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const params = require('../config/params');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('blog_posts', [
+      {
+        title: 'Cafe XXX',
+        content: 'cafe for adults only',
+        slug: 'cafe-xxx',
+        area: params.blogPost.area.umeda.id,
+        image: '',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'Cafe DOOM',
+        content: 'The cafe where you spend the last day of your life',
+        slug: 'cafe-doom',
+        area: params.blogPost.area.namba.id,
+        image: '',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'Cafe Socrates',
+        content: 'All customers will be enlightened',
+        slug: 'cafe-socrates',
+        area: params.blogPost.area.yodoyabashi.id,
+        image: '',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('products', null, {});
+  }
+}
